5d878dfe62ca3bc93a18b7a764273f5c0dc7c1c8
fatimah23vm <ifatemah.23@icloud.com>
2023-11-26 11:55:06 +0300

    Initial Commit

---
+++ fashoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fashoin
+//
+//  Created by Fatimah Adeeb on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fashoin/fashoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  fashoinApp.swift
+//  fashoin
+//
+//  Created by Fatimah Adeeb on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct fashoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
